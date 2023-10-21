c523753050f9780a958f4b1b70823d182259f842
TandinPeljor <03230324.gcbs@rub.edu.bt>
2023-10-21 22:16:54 +0600

    changed the bg in line 10

--- Hangman_game.py
+++ Hangman_game.py
@@ -7,6 +7,6 @@ while run:
     root = Tk()
     root.geometry('905x700')
     root.title('Hangman')
-    root.config(bg = '#E7FFFF')
+    root.config(bg = '#F0F8FF')
     count = 0
     win_count = 0
