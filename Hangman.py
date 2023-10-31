34b76e72448e6cd567b2a487b69853a7c0634e33
TandinPeljor <03230324.gcbs@rub.edu.bt>
2023-10-31 21:58:22 +0600

    button variables

--- Hangman.py
+++ Hangman.py
@@ -8,6 +8,17 @@ WIDTH, HEIGHT = 800, 500 #setting the width (800 pixels) and height (500 pixels)
 win = pygame.display.set_mode((WIDTH, HEIGHT)) #This function creates the game window with the specified width and height
 pygame.display.set_caption("Hangman Game") #This function sets the window title to "Hangman Game" 
 
+# Button Variables
+RADIUS = 20
+GAP = 15
+letters = []
+startx = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2) 
+starty = 400
+for i in range(26):
+    x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
+    y = starty + ((i // 13) * (GAP + RADIUS * 2))
+    letters.append([x, y])
+
 
 # Load Images 
 images = [] # Creates an empty list named images
@@ -16,11 +27,11 @@ for i in range(7): # Starts a for loop that will iterate 7 times, with i taking 
     images.append(image) # This line appends the loaded image to the images list. After the loop finishes, images will be a list of seven Pygame Surface objects, each representing one of the loaded images
 
 # Game Variables
-hangman_status = 5
+hangman_status = 6
 
 # colors
 WHITE = (255,255,255) # This creates a tuple representing the color white. Pygame uses RGB values to represent colors, where (255,255,255) corresponds to white
-
+BLUE = (0, 0, 255)
 
 # Setup Game Loop
 
@@ -28,16 +39,27 @@ FPS = 60 # Setting the frames per second (FPS) for the game. This is the number 
 clock = pygame.time.Clock() # This creates a clock object that you can use to control the game's framerate.
 run = True # This is a flag variable used to control the main game loop. As long as this variable is True, the game loop will keep running.
 
-while run:  # This is the main game loop. Everything inside this loop will be executed every frame.
-    clock.tick(FPS) # This function makes sure that your game runs at the FPS, specified earlier. It will delay the game to ensure it runs at the correct speed
-    
+def draw():
     win.fill((WHITE)) #  fills the entire game window with white color.
+    
+    # Draw buttons
+    for letter in letters:
+        x, y = letter
+        pygame.draw.circle(win, BLUE, (x, y), RADIUS, 3) 
+    
     win.blit(images[hangman_status], (150, 100)) # draws the current Hangman image onto the window at the position (150, 100)
     pygame.display.update() #  updates the entire screen to display the new content 
     
+
+
+while run:  # This is the main game loop. Everything inside this loop will be executed every frame.
+    clock.tick(FPS) # This function makes sure that your game runs at the FPS, specified earlier. It will delay the game to ensure it runs at the correct speed
+    
+    draw()
 
     for event in pygame.event.get(): # This is an event loop. It checks for events such as keyboard presses or mouse movement.
         if event.type == pygame.QUIT: # This checks if the event is a QUIT event, which happens when the user clicks the close button on the game window.
             run = False # If the user clicked the close button, this sets the run variable to False, which will stop the main game loop.
             
 pygame.quit() # This function is called after the main game loop has ended. It will cleanly exit the Pygame library
+
